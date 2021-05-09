f302103e4a67a4c39aba22a730d43d782b832ad4
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-05-09 23:46:12 +0530

    Detect Cycle Leetcode

---
+++ detectcyclell.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode(int x) : val(x), next(NULL) {}
+  };
+bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        while(fast and fast->next){
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if(fast == slow){
+                return true;
+            }
+        }
+        
+        return false;
+    }
