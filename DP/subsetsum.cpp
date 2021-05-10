89413a787c3341097a683a58fd10944ece477cee
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-05-10 23:57:21 +0530

    Subset Sum Memoized

--- DP/subsetsum.cpp
+++ DP/subsetsum.cpp
@@ -6,18 +6,22 @@ bool getSubset(vector<int> &nums,int target, int i){
     if(target == 0){
         return true;
     }
+    if(dp[i][target] != -1){
+        return dp[i][target];
+    }
     if(i == 0){
         return false;
     }
     if(nums[i] > target){
-        return getSubset(nums,target,i-1);
+        dp[i][target] = getSubset(nums,target,i-1);
     }
     else{
-        return getSubset(nums,target,i-1) || getSubset(nums,target-nums[i],i-1);
+        dp[i][target] = getSubset(nums,target,i-1) || getSubset(nums,target-nums[i],i-1);
     }
+    return dp[i][target];
 }
 bool subsetSum(vector<int> &nums,int target){
-    memset(dp,0,sizeof(dp));
+    memset(dp,-1,sizeof(dp));
     
     return getSubset(nums,target,nums.size()-1);
 
@@ -25,7 +29,7 @@ bool subsetSum(vector<int> &nums,int target){
 int main(){
     vector<int> nums;
     nums.push_back(1);
-    // nums.push_back(2);
+    nums.push_back(2);
     nums.push_back(3);
     nums.push_back(5);
     cout<<subsetSum(nums,7)<<endl;
