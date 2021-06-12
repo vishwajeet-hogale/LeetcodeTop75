9fbc0c2fb8f8a4f076bd490963791a56596c74ef
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-06-13 00:32:36 +0530

    Bottom up levels

---
+++ DailyCode/reverseLevel.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+using namespace std;
+struct TreeNode{
+    int val;
+    TreeNode* left,*right;
+};
+vector<vector<int>> levelOrderTraversal(TreeNode *root){
+    vector<vector<int>> res;
+    queue<TreeNode*> q;
+    q.push(root);
+    while(!q.empty()){
+        int size = q.size();
+        vector<int> temp;
+        for(int i=0;i<size;i++){
+            
+            TreeNode *curr = q.front();
+            q.pop();
+            if(curr==NULL)
+                continue;
+            temp.push_back(curr->val);
+            if(curr->left)
+                q.push(curr->left);
+            if(curr->right)
+                q.push(curr->right);
+            
+        }
+        res.push_back(temp);
+    }
+    reverse(res.begin(),res.end());
+    return res;
+}
