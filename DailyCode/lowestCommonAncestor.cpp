503395003056741b4528112262525f54fa2af5f1
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2021-06-12 16:13:45 +0530

    Lowest Common Ancestor

---
+++ DailyCode/lowestCommonAncestor.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+     int val;
+     TreeNode *left,*right;
+};
+int a1 = 0,b1 = 0;TreeNode* prev = NULL;
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q)
+    if (!root || root == p || root == q) return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if (left && right) return root;
+        return left ? left : right;
+}
