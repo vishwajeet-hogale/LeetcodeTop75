673d2793a0fcb782ea18352ba649a0e8ef6f9a0f
Vishwajeet Shivaji Hogale <vishwajeethogale307@gmail.com>
2022-03-05 02:21:16 +0530

    Deepest leaves sum

---
+++ Brushup/deepestLeavesSum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+struct treenode{
+    int data;
+    treenode *left;
+    treenode *right;
+};
+
+int deepestSum(){
+    queue<treenode*> q;
+    
+}
